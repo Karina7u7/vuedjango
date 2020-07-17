a12bba11dce44849d73e16180415c8e81500fa59
Carina <karinanime1999@gmail.com>
2020-07-17 14:46:35 -0500

    crea archivos iniciales del proyecto

---
+++ src/components/ListType.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+       <ListDefault :elements-list="elements" />
+    </div>
+</template>
+
+<script>
+import ListDefault from '../partials/_ListDefault';
+
+export default {
+    components:{
+        ListDefault
+
+    },
+    created(){
+       this.findAll();
+    },
+     data(){
+         return{           
+             elements: [ ]
+         };
+     },
+     methods: {
+         findAll: function(){
+            fetch('http://127.0.0.1:8000/api/type/'+this.$route.params.id+'/elements/?format=json')
+            .then(res => res.json())
+            .then(res => this.elements = res) 
+         }
+     },
+     watch:{
+         "$route.params.id" : function(){
+            console.log("listado de categorias ")
+            this.findAll();
+
+         }
+     }
+};
+</script>
+<style>
+.box {
+    border: 3px solid #ccc;
+    margin: 5px 0 0 0;
+}
+
+</style>
